0deed2f2a9206662f4b1237a42021d3bee09de10
DIVYANSHU <jeetarget2025@gmail.com>
2025-07-02 18:10:34 +0530

    02-07-2025( 18:10 )

--- mapping.js
+++ mapping.js
@@ -1,4 +1,4 @@
-let username = prompt("Enter your name:");
+let username = pompt("Enter your name:");
 // This code prompts the user to enter their name and converts it to uppercase.
 
 
@@ -36,25 +36,3 @@ let profileimage = {
 
 
 
-if (username) {
-
-
-    let a = username.charAt(0).toUpperCase();
-    // This code extracts the first character of the username.
-
-    let logo = document.querySelector(".logo img");
-    //IT IS USED TO SELECT THE LOGO IMAGE ELEMENT.
-
-
-    let image = profileimage[a] || "1.jpeg" ;
-    // This code checks if the first character of the username exists in the profileimage object.
-    // If it exists, it assigns the corresponding image path to the variable 'image'.
-
-    document.querySelector("#pic").src = image;
-
-}
-
-else {
-    document.querySelector(".logo img").src = "1.jpeg";
-    // If the first character of the username does not match any key in the profileimage object, it sets the logo image to "1.jpeg".    
-}
