9399f650900833244b30f4da823fcab6b67f45cc
DIVYANSHU <jeetarget2025@gmail.com>
2025-07-04 11:08:58 +0530

    04-07-2025 ( 11:08)

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let info = document.querySelector(".info");
+let button = document.querySelector(".profile-pic");
+let biousername = document.querySelector("#bio-username")
+
+button.addEventListener("click", () => {
+    info.style.display = "block";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target !== button && event.target !== info) {
+        info.style.display = "none";
+    }
+
+});
+
+ let username = prompt("ENTER YOUR NAME");
+
+function checkusername() {
+    
+
+    if (username === null || username === "") {
+        // handle empty or cancelled input here
+        checkusername()
+    }
+}
+
+
+checkusername()
+
+let usernamelower = username.toLowerCase
+let length = username.length
+let a = biousername.innerHTML = ""  
+
+console.log(length)
