1553642b9611421ca9cad67f0c76c18d398acd99
DIVYANSHU <jeetarget2025@gmail.com>
2025-07-10 17:30:48 +0530

    10-07-2025( 17:30 )

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ let button = document.querySelector(".profile-pic");
 let biousername = document.querySelector("#bio-username")
 let bioname = document.querySelector("#bio-name")
 
-button.addEventListener("click", () => {
-    info.style.display = "block";
-});
+if (button && info) {
+    button.addEventListener("click", () => {
+        info.style.display = "block";
+    });
 
-window.addEventListener("click", (event) => {
-    if (event.target !== button && event.target !== info) {
-        info.style.display = "none";
-    }
-
-});
-
+    window.addEventListener("click", (event) => {
+        if (event.target !== button && event.target !== info) {
+            info.style.display = "none";
+        }
+    });
+}
 
 let nameValue = localStorage.getItem("name");
 let usernameValue = localStorage.getItem("username");
 
-
-
-
-biousername.innerHTML = usernameValue
-bioname.innerHTML = nameValue
+if (biousername) {
+    biousername.innerHTML = usernameValue;
+}
+if (bioname) {
+    bioname.innerHTML = nameValue;
+}
